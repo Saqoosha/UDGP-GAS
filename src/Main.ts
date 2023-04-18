4cbc139e3ae9463697c87553adfc143bea26e9e5
Saqoosha <a@saqoo.sh>
2023-04-19 00:46:33 +0900

    Move

--- src/Main.ts
+++ src/Main.ts
@@ -1,36 +1,3 @@
-const pilotsSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("参加パイロット");
-const heatListSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("組み合わせ / タイムスケジュール");
-
-function setRound1Heats() {
-    const range = pilotsSheet.getRange("A2:A");
-    const pilots = range.getValues().filter((v) => v[0] !== "").map((v) => v[0] as string);
-
-    const CHANNEL_NAMES = ["E1 5705", "F1 5740", "F4 5800"];
-    const channels = pilots.map((pilot, i) => CHANNEL_NAMES[i % 3]);
-    pilotsSheet.getRange(2, 2, channels.length, 1).setValues(channels.map((channel) => [channel]));
-
-    const heats = pilots.reduce((acc, pilot, i) => {
-        const index = Math.floor(i / 3);
-        if (!acc[index]) {
-            acc[index] = [];
-        }
-        acc[index].push(pilot);
-        return acc;
-    }, []);
-    const lastHeat = heats[heats.length - 1];
-    switch (lastHeat.length) {
-        case 1:
-            lastHeat.unshift(heats[heats.length - 2].pop() as string);
-            lastHeat.push("");
-            heats[heats.length - 2].push("");
-            break;
-        case 2:
-            lastHeat.push("");
-            break;
-    }
-    heatListSheet.getRange(2, 4, heats.length, 3).setValues(heats);
-}
-
 function getCurrentRound() {
     const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("data");
     const value = sheet.getRange("B2").getValue();
--- src/Race1.ts
+++ src/Race1.ts
@@ -1,3 +1,36 @@
+const heatListSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("組み合わせ / タイムスケジュール");
+
+function setRound1Heats() {
+    const pilotsSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("参加パイロット");
+    const range = pilotsSheet.getRange("A2:A");
+    const pilots = range.getValues().filter((v) => v[0] !== "").map((v) => v[0] as string);
+
+    const CHANNEL_NAMES = ["E1 5705", "F1 5740", "F4 5800"];
+    const channels = pilots.map((pilot, i) => CHANNEL_NAMES[i % 3]);
+    pilotsSheet.getRange(2, 2, channels.length, 1).setValues(channels.map((channel) => [channel]));
+
+    const heats = pilots.reduce((acc, pilot, i) => {
+        const index = Math.floor(i / 3);
+        if (!acc[index]) {
+            acc[index] = [];
+        }
+        acc[index].push(pilot);
+        return acc;
+    }, []);
+    const lastHeat = heats[heats.length - 1];
+    switch (lastHeat.length) {
+        case 1:
+            lastHeat.unshift(heats[heats.length - 2].pop() as string);
+            lastHeat.push("");
+            heats[heats.length - 2].push("");
+            break;
+        case 2:
+            lastHeat.push("");
+            break;
+    }
+    heatListSheet.getRange(2, 4, heats.length, 3).setValues(heats);
+}
+
 function calcRace1Result() {
     const lock = LockService.getDocumentLock();
     lock.waitLock(20000);
@@ -157,7 +190,6 @@ function addPilotResultsForRace1(pilot: string, records: RoundRecord[]) {
 
 function setRace1NextRoundHeats(nextRound: number, prevRoundResults: RoundRecord[]) {
     const pilots = prevRoundResults.sort((a, b) => a.fastestLapTime - b.fastestLapTime).map(record => record.pilot);
-    // console.log({ prevRoundResults, pilots });
     const heats = pilots.reduce((acc, pilot, i) => {
         const index = Math.floor(i / 3);
         if (!acc[index]) {
