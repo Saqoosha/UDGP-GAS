c8dc0961b4f8fdc65ae9629ba34f3c805fcbd8df
Saqoosha <a@saqoo.sh>
2024-03-11 00:45:12 +0900

    fix...

--- src/Race2.ts
+++ src/Race2.ts
@@ -1,5 +1,5 @@
 function clearRace2RawResult() {
-    const sheet = race1ResultSheet;
+    const sheet = race2ResultSheet;
     sheet.getRange("A2:AK").clearContent();
     sheet.getRange("G2:G").setValue(false);
     sheet.getRange("H2:H").setValue("=IF(G2=TRUE, E2-2, E2)");
